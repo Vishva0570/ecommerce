1b113d58fae64324635d4adb66e6cbf965663877
Vishva0570 <vishvavsvs@gmail.com>
2024-10-18 18:01:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ECOMMERCE
+- it can be implemented in springboot
+- java version 21
+- front end can be done in react
+- to run this project you can use the command `./mvnw spring-boot:run`
---
+++ src/main/java/com/example/e_commerce/Hello.java
@@ -0,0 +1,18 @@
+package com.example.e_commerce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping
+public class Hello {
+    @GetMapping("/")
+    public String Greet() {
+        return "Hello world";
+    }
+    
+}
